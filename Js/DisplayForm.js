09c00b090c7fcdae250eed2b74426591f07d031c
AnshulRathore <anshulrathore1995@gmail.com>
2022-06-24 09:56:55 +0530

    Added:Data From Local Storage In Home Page

--- Js/DisplayForm.js
+++ Js/DisplayForm.js
@@ -1,26 +1,35 @@
+let addrPayrollList
 window.addEventListener('DOMContentLoaded', (event) => {
+    addrPayrollList =   getEmployeePayrollDataFromStorage();
+    document.querySelector(".per-count").textContent = addrPayrollList.length;
     createInnerHtml();
 });
 
+getEmployeePayrollDataFromStorage= () => {
+    return localStorage.getItem('AddressBookList') ? 
+                    JSON.parse(localStorage.getItem('AddressBookList')) : [];
+}
+
 const createInnerHtml = () => {
-    const HeaderHtml = "<tr><th>Full Name</th><th>Address</th><th>City</th><th>State</th>" +
-    "<th>Phone No</th><th>Zip Code</th><th>Options</th></tr>";
-    const innerHtml = `${HeaderHtml}
-        
-                <tr>
-                    <td>Anshul Rathore</td>
-                    <td>1 D 23 talwandi</td>
-                    <td>
-                       Kota
-                    </td>
-                    <td>Rajasthan</td>
-                    <td>91 8764314432</td>
-                    <td>324 005</td>
-                    <td>
-                        <img id="1" onclick="remove(this)" alt="delete" src="/Assets/icons/delete-black-18dp.svg">
-                        <img id="1" alt="edit" onclick="update(this)" src="/Assets/icons/create-black-18dp.svg">
-                    </td>
-                </tr>
-                `;
+    if (addrPayrollList.length == 0) return;
+    const headerHtml = "<tr><th>Full Name</th><th>Address</th><th>City</th><th>State</th>" +
+        "<th>Phone No</th><th>Zip Code</th><th>Options</th></tr>";
+    let innerHtml = `${headerHtml}`;
+    for (const addrPayrollData of addrPayrollList) {
+        innerHtml = `${innerHtml}
+            <tr>
+                <td>${addrPayrollData.name}</td>
+                <td>${addrPayrollData.address}</td>
+                <td>${addrPayrollData.city}</td>
+                <td>${addrPayrollData.state}</td>
+                <td>${addrPayrollData.phone}</td>
+                <td>${addrPayrollData.zipcode}</td>
+                <td>
+                    <img id="1" onclick="remove(this)" alt="delete" src="/Assets/icons/delete-black-18dp.svg">
+                    <img id="1" alt="edit" onclick="update(this)" src="/Assets/icons/create-black-18dp.svg">
+                </td>
+            </tr>
+        `;
+    }
     document.querySelector('#display').innerHTML = innerHtml;
 }
